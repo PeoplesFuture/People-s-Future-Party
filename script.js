a8aa8e758a612badd5cab8eacdef58226f748a4b
PeoplesFuture <info.peoplesfuture@gmail.com>
2025-03-01 09:50:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,13 @@ function toggleMenu() {
 
 
 // Main Slider
-let slideIndex = 0;
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // เปลี่ยนภาพทุก 3 วินาที
+function showNextSlide() {
+  slides[currentIndex].classList.remove('fade');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('fade');
 }
 
-document.addEventListener("DOMContentLoaded", showSlides);
+setInterval(showNextSlide, 3000); // Change slide every 3 seconds
